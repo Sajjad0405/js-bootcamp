140c5064d161b0888bb3ba04c525bd9b4e376e46
Sajjad0405 <43657989+Sajjad0405@users.noreply.github.com>
2019-02-07 09:12:33 +0100

    Update l2-bean-counting.js

--- l2-bean-counting.js
+++ l2-bean-counting.js
@@ -1,1 +1,13 @@
- 
+ var countBs = function(str) {
+    return str.match(/B/g).length;
+};
+
+var countChar = function(str, character) {
+    var matchLetter = new RegExp(character, 'g');
+    return str.match(matchExp).length;
+};
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4

d5bacf5b0163c2a55fba19386061cee83d1ec880
Sajjad0405 <43657989+Sajjad0405@users.noreply.github.com>
2019-02-11 14:23:34 +0100

    Update l3-flattening.js

--- l3-flattening.js
+++ l3-flattening.js
@@ -1,1 +1,8 @@
- 
+ let array = [["My", "name", "is"], ["Sajjad", "Ali"]];
+
+console.log(array.reduce( (a, b) => a.concat(b) ));
+
+
+let array2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9, 10]];
+
+console.log(array2.reduce( (a, b) => a.concat(b)));

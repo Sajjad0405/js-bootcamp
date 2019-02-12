817af16b1ffc5d74fc4f73d6a32b2cd21569dabf
Sajjad0405 <43657989+Sajjad0405@users.noreply.github.com>
2019-02-12 13:54:44 +0100

    Update l1-chessboard.js

--- l1-chessboard.js
+++ l1-chessboard.js
@@ -2,14 +2,19 @@ var size = 8;
 
 var board = "";
 
-for (var y = 0; y < size; y++) {   
+//loop 8 maal via de forloop
+for (var y = 0; y < size; y++) {  
+  
   for (var x = 0; x < size; x++) {
       
+    //wanneer door 2 te delen, maak een lege plek aan
     if ((x + y) % 2 == 0)
       board += " ";
     else
+      //maak anders een # aan
       board += "#";
   }
+  //Voeg een break toe voor elke keer dat er geloopt wordt
   board += "\n";
 }
 

39b3e405d4bc4462b088377429a04e8edf195c81
Sajjad0405 <43657989+Sajjad0405@users.noreply.github.com>
2019-02-16 00:37:02 +0100

    Update l2-sum-of-a-range.js

--- l2-sum-of-a-range.js
+++ l2-sum-of-a-range.js
@@ -1,1 +1,16 @@
 
+function range(start, end, step = start < end ? 1 : -1) {
+  var array = [];
+
+if (step > 0) {
+    for (var i = start; i <= end; i += step) array.push(i);
+}
+else {
+    for (var i = start; i >= end; i += step) array.push(i);
+}
+    return array;
+}
+
+
+console.log(range(1, 10))
+console.log(range(5, 2, -1));
